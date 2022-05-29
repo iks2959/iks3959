8a7d3d723cab41e5e2f0f1877c2cdff10d065c12
unknown <iks2959@naver.com>
2022-05-29 23:43:27 +0900

    header update 05

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 document.querySelector('.navbar_togleBtn').addEventListener('click',function(){
     document.querySelectorAll('.nav_menu')[0].classList.toggle('show');
    });
+
+   var 슬라이드 = 1;
+
+   $('.right').on('click',function(){
+     if(슬라이드 == 1){
+       $('.slide-container').css('transform', 'translateX(-100vw)');
+       슬라이드 += 1
+     } else if (슬라이드 = 2) {
+       $('.slide-container').css('transform', 'translateX(-200vw)');
+       슬라이드 += 1
+     } else if (슬라이드 = 3) {
+       $('.slide-container').css('transform', 'translateX(-300vw)');
+     }
+   })
+
+   $('.left').on('click',function(){
+    if(슬라이드 == 3){
+      $('.slide-container').css('transform', 'translateX(-100vw)');
+      슬라이드 -= 1
+    } else if (슬라이드 = 2) {
+      $('.slide-container').css('transform', 'translateX(-0vw)');
+      슬라이드 -= 1
+    }
+  })
