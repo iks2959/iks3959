d353c8fe827f53205827c6539d97d1fa1fd23950
unknown <iks2959@naver.com>
2022-05-31 20:31:36 +0900

    section update

--- main.js
+++ main.js
@@ -25,3 +25,15 @@ document.querySelector('.navbar_togleBtn').addEventListener('click',function(){
       슬라이드 -= 1
     }
   })
+
+  $('#slide > div:gt(0)').hide();
+
+setInterval(function(){
+    $('#slide > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slide');
+},3000);
+
